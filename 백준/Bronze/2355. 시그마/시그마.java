fb002294640541e0aa26a922c830785a55f5c4d4
kcrikana <83369769+kcrikana@users.noreply.github.com>
2024-01-14 15:50:08 +0900

    [Bronze II] Title: 시그마, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/2355. 시그마/시그마.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+        long A = Math.min(N,M);
+        long B = Math.max(N,M);
+        long sum = (((B + 1) * B) / 2) - (((A - 1) * A) / 2);
+        System.out.println(sum);
+    }
+}
