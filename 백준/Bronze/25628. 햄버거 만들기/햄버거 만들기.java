2516a0874e1fcad4a0fb653321416685ad95c843
kcrikana <83369769+kcrikana@users.noreply.github.com>
2024-03-09 14:01:31 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 80 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        System.out.println(Math.min(A/2,B));
+    }
+}
