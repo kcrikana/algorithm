f0a8da6718e64f75669a2bdc712d336ddaf1826c
kcrikana <83369769+kcrikana@users.noreply.github.com>
2024-05-12 22:09:44 +0900

    [level 3] Title: 아방가르드 타일링, Time: 12.29 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    static final long MOD = 1000000007;
+    public int solution(int n) {
+        // 원래 dp배열
+        long[] dp = new long[100001];
+        // 누적합임
+        long[] dp2 = new long[100001];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 3;
+        dp[3] = 10;
+        dp[4] = 23;
+        dp[5] = 62;
+        dp[6] = 170;
+        dp2[0] = 1;
+        dp2[1] = 1;
+        dp2[2] = 3;
+        dp2[3] = 11;
+        dp2[4] = 24;
+        dp2[5] = 65;
+        dp2[6] = 181;
+        // 3개씩 늘어날때마다 누적합으로 계산
+        for(int i=7; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]*2 + dp[i-3]*5 + dp2[i-4]*2 + dp2[i-5]*2 + dp2[i-6]*4)%MOD;
+            dp2[i] = (dp[i] + dp2[i-3])%MOD;
+        }
+        return Math.toIntExact(dp[n]);
+    }
+}
