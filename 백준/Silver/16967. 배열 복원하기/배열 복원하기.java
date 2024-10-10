8f220a8f67de3de2d38cab321a91559f95a88b29
kcrikana <83369769+kcrikana@users.noreply.github.com>
2024-10-11 01:12:47 +0900

    [Silver III] Title: 배열 복원하기, Time: 248 ms, Memory: 26916 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		int Y = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
+		int[][] A = new int[H][W];
+		for(int i=0; i<H+X; i++) {
+			st = new StringTokenizer(bf.readLine());
+			if(i>=H) continue;
+			for(int j=0; j<W; j++) {
+				if(i<X && j<W) {
+					A[i][j] = Integer.parseInt(st.nextToken());
+				}
+				else if(i>=X){
+					if(j<Y) A[i][j] = Integer.parseInt(st.nextToken());
+					else A[i][j] = Integer.parseInt(st.nextToken())-A[i-X][j-Y];
+				}
+			}
+		}
+		for(int i=0; i<H; i++) {
+			for(int j=0; j<W; j++) {
+				sb.append(A[i][j]).append(' ');
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+}
