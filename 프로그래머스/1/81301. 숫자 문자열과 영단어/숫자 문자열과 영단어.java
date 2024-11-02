eb7014ea1be5a19ff658c6e506682eedd4403bce
kcrikana <83369769+kcrikana@users.noreply.github.com>
2024-11-03 05:54:19 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 1.77 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,36 +1,11 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("zero", 0);
-        map.put("one", 1);
-        map.put("two", 2);
-        map.put("three", 3);
-        map.put("four", 4);
-        map.put("five", 5);
-        map.put("six", 6);
-        map.put("seven", 7);
-        map.put("eight", 8);
-        map.put("nine", 9);
-        StringBuilder answer = new StringBuilder();
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<s.length(); i++) {
-            char ch = s.charAt(i);
-            int num = s.charAt(i)-'0';
-            if(num >= 0 && num <= 9) {
-                answer.append(num);
-                sb = new StringBuilder();
-            }
-            else {
-                sb.append(ch);
-                if(map.containsKey(sb.toString())) {
-                    int now = map.get(sb.toString());
-                    answer.append(now);
-                    sb = new StringBuilder();
-                }
-            }
+        int answer = 0;
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i=0; i<=9; i++) {
+            s = s.replaceAll(arr[i], Integer.toString(i));
         }
-        int result = Integer.parseInt(answer.toString());
-        return result;
+        return Integer.parseInt(s);
     }
 }
