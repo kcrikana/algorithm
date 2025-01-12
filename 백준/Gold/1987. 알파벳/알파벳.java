2b82cf8c675cb1b7f3160481d7a851a3297c86c8
kcrikana <83369769+kcrikana@users.noreply.github.com>
2025-01-12 16:35:38 +0900

    [Gold IV] Title: 알파벳, Time: 1060 ms, Memory: 12692 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	static int R, C, ans;
+	static char[][] arr;
+	static boolean[][] visited;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+
+	static int num = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		arr = new char[R][C];
+		visited = new boolean[R][C];
+		for(int i=0; i<R; i++) arr[i] = br.readLine().toCharArray();
+		num += 1 << (arr[0][0]-'A');
+		visited[0][0] = true;
+		ans = 0;
+		dfs(0,0);
+		System.out.println(ans);
+	}
+
+	static void dfs(int row, int col) {
+		ans = Math.max(ans, Integer.bitCount(num));
+		for(int i=0; i<4; i++) {
+			int dRow = dr[i] + row;
+			int dCol = dc[i] + col;
+			if(dRow < 0 || dCol < 0 || dRow >= R || dCol >= C || visited[dRow][dCol] || (num & 1 << (arr[dRow][dCol]-'A')) != 0) continue;
+			int now = 1 << (arr[dRow][dCol]-'A');
+			num += now;
+			visited[0][0] = true;
+			dfs(dRow, dCol);
+			visited[0][0] = false;
+			num -= now;
+		}
+	}
+}
