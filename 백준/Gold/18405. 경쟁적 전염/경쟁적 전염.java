68f717c63420d32f225e856ebf473dcbbe6a06c5
kcrikana <83369769+kcrikana@users.noreply.github.com>
2025-04-09 21:03:29 +0900

    [Gold V] Title: 경쟁적 전염, Time: 576 ms, Memory: 208572 KB -BaekjoonHub

---
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	static LinkedList<Node> list = new LinkedList<>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[][] examiner = new int[N][N];
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++) {
+				examiner[i][j] = Integer.parseInt(st.nextToken());
+				list.add(new Node(i,j, examiner[i][j], 0));
+			}
+		}
+		st = new StringTokenizer(br.readLine());
+		int S = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		int Y = Integer.parseInt(st.nextToken());
+		Collections.sort(list, (o1, o2) -> (o1.virus-o2.virus));
+		while(!list.isEmpty()) {
+			Node now = list.poll();
+			if(now.time == S) break;
+			for(int k=0; k<4; k++) {
+				int dRow = dr[k] + now.row;
+				int dCol = dc[k] + now.col;
+				if(dRow < 0 || dCol < 0 || dRow >= N || dCol >= N
+					|| examiner[dRow][dCol] != 0) continue;
+				examiner[dRow][dCol] = now.virus;
+				list.add(new Node(dRow, dCol, now.virus, now.time+1));
+			}
+		}
+		System.out.println(examiner[X-1][Y-1]);
+	}
+	static class Node {
+		int row, col, virus, time;
+		public Node(int row, int col, int virus, int time) {
+			this.row = row;
+			this.col = col;
+			this.virus = virus;
+			this.time = time;
+		}
+	}
+}
