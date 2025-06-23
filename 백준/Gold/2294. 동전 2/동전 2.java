9ebc8ffc8581df93613056e0ff9cf4ff9efc8d3f
kcrikana <83369769+kcrikana@users.noreply.github.com>
2025-06-23 19:05:48 +0900

    [Gold V] Title: 동전 2, Time: 84 ms, Memory: 11868 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,26 +1,27 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		long[] dp = new long[k+1];
+		int[] dp = new int[k+1];
 		int[] arr = new int[n];
-		Arrays.fill(dp, 200000000000l);
+		Arrays.fill(dp,2000000000);
 		for(int i=0; i<n; i++) {
 			int num = Integer.parseInt(br.readLine());
-			if(num <= k) dp[num] = 1l;
+			if(num <= k) dp[num] = 1;
 			arr[i] = num;
 		}
 		for(int i=1; i<=k; i++) {
 			for(int j=0; j<n; j++) {
 				int pastIdx = i-arr[j];
-				if(pastIdx >= 1) dp[i] = Math.min(dp[i], dp[pastIdx]+1l);
+				if(pastIdx >= 1) dp[i] = Math.min(dp[i], dp[pastIdx]+1);
 			}
 		}
-		System.out.println(dp[k] == 200000000000l ? -1l : dp[k]);
+		System.out.println(dp[k] == 2000000000 ? -1 : dp[k]);
 
 	}
 }
