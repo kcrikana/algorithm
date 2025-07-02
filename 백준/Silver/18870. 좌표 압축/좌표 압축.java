c2bb20f252394d2e4f4fda603aa9fc0d212d224c
kcrikana <83369769+kcrikana@users.noreply.github.com>
2025-07-03 01:55:02 +0900

    [Silver II] Title: 좌표 압축, Time: 1648 ms, Memory: 359248 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -6,16 +6,28 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		TreeMap<Integer, Integer> map = new TreeMap<>();
-		int[] arr = new int[N];
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			map.put(arr[i], 0);
+		int[] origin = new int[N];
+		int[] sorted = new int[N];
+		HashMap<Integer, Integer> rankingMap = new HashMap<>();
+
+		for(int i = 0; i < N; i++) {
+			sorted[i] = origin[i] = Integer.parseInt(st.nextToken());
 		}
-		int idx = 0;
-		for(int key : map.keySet()) map.put(key, idx++);
+
+		Arrays.sort(sorted);
+
+		int rank = 0;
+		for(int v : sorted) {
+			if(!rankingMap.containsKey(v)) {
+				rankingMap.put(v, rank);
+				rank++;
+			}
+		}
+
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<N; i++) sb.append(map.get(arr[i])).append(' ');
+		for(int key : origin) {
+			sb.append(rankingMap.get(key)).append(' ');
+		}
 		sb.delete(sb.length()-1, sb.length());
 		System.out.println(sb);
 	}
